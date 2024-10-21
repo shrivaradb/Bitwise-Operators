94cef3bdc0eddb943ff0b1a0471896e4ae203598
shrivaradb <shrivarad.bhagwatikar@gmail.com>
2024-10-21 23:56:27 +0530

    Add files via upload

---
+++ bitwise_operators.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    a=3;
+    b=5;
+
+   //(bitwise AND)
+    printf("a&b=%d\n",a&b);
+
+   //(bitwise OR)
+    printf("a|b=%d\n",a|b);
+
+ //(bitwise XOR)
+    printf("a^b=%d\n",a^b);
+
+    return 0;
+}
